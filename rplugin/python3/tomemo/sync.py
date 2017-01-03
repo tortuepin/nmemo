afb96ab9d0afb897070b4a86a67599614671c1d6
tortuepin <kameyasu119@gmail.com>
2017-01-03 20:40:07 +0900

    sync

--- rplugin/python3/tomemo/sync.py
+++ rplugin/python3/tomemo/sync.py
@@ -1,8 +1,33 @@
-import neovim
 
-@neovim.plugin
-class SyncSimpleNote(object):
+'''
+getUserData
+arg
+    filename : filename of userdata
+ret
+    dictonary: {"email":'', "password":''}
 
-    def __init__(selfm nvim):
-        self.nvim = nvim
+datafile should be this structure : 
+1. email
+2. password
+number and '.' which is in head represent linenumber.
+so you don't need write it in datafile.
+'''
+def getUserData(filename):
+    data = {"email":'', "username":''}
+    f = open(filename, 'r')
+    data["email"] = f.readline().strip()
+    data["password"] = f.readline().strip()
+    
+    return data
 
+'''
+fetch AuthData
+arg
+    email
+    password
+ret
+    authData : simplenote object
+'''
+def fetchAuthData(email, password):
+    authData = simplenote.Simplenote(email, password)
+    return authData
