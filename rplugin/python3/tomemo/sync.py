0396d1f370f82da0547c27b1b436161155335950
tortuepin <kameyasu119@gmail.com>
2017-01-08 11:52:05 +0900

    sync make note list

--- rplugin/python3/tomemo/sync.py
+++ rplugin/python3/tomemo/sync.py
@@ -1,18 +1,18 @@
+import simplenote
+def get_User_Data(filename):
+    '''
+    get user data from file
 
-'''
-getUserData
-arg
-    filename : filename of userdata
-ret
-    dictonary: {"email":'', "password":''}
+    ret
+        dictonary: {"email":'', "password":''}
 
-datafile should be this structure : 
-1. email
-2. password
-number and '.' which is in head represent linenumber.
-so you don't need write it in datafile.
-'''
-def getUserData(filename):
+    datafile should be this structure : 
+    1. email
+    2. password
+    number and '.' which is in head represent linenumber.
+    so you don't need write it in datafile.
+    '''
+
     data = {"email":'', "username":''}
     f = open(filename, 'r')
     data["email"] = f.readline().strip()
@@ -20,14 +20,35 @@ def getUserData(filename):
     
     return data
 
-'''
-fetch AuthData
-arg
-    email
-    password
-ret
-    authData : simplenote object
-'''
-def fetchAuthData(email, password):
+def fetch_Auth_Data(email, password):
+    '''
+    fetch authdata from the internet.
+    '''
     authData = simplenote.Simplenote(email, password)
     return authData
+
+def fetch_tomemo_index(authData):
+    '''
+    fetch note about tomemo from the internet
+    '''
+    return authData.get_note_list(tags=["tomemo"])
+
+def make_note_list(authData):
+    '''
+    make tomemo note list.
+    {"title":"content"}
+    '''
+    index = fetch_tomemo_index(authData)
+    titleList = {}
+
+    for noteIndex in index[0]:
+        noteData = authData.get_note(noteIndex['key'])[0]
+        title = noteData['content'].split("\n")[0]
+        titleList[title] = noteData['content']
+
+    return titleList
+
+
+
+
+
